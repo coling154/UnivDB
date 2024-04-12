56fbe318702eee92fc77c3e0ecd300260b84ff59
Merpster <ryan.quirk6@protonmail.ch>
2024-04-12 11:08:29 -0400

    added templates back

--- src/univSite/univSite/settings.py
+++ src/univSite/univSite/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+f = open("univSite/file.txt",'r')
+pwd = f.read(8)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'univSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'university',
+        'USER': 'root',
+        'PASSWORD': pwd,
+        'HOST': 'localhost'
     }
 }
 
