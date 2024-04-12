2ec5299b4f9a795e8ab272a9c5c3c076ba7f0a6a
Merpster <ryan.quirk6@protonmail.ch>
2024-04-12 15:01:32 -0400

    Connected landing pages to main index page at localhost

--- src/univSite/univApp/urls.py
+++ src/univSite/univApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("admin/", views.admin, name="admin"),
     path("professor/", views.professor, name="professor"),
     path("student/", views.student, name="student"),
