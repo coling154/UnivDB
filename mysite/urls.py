bff02819b55e18b2f1330713d9e847e4e4febd7f
Colin Gasiewicz <gasiewca@clarkson.edu>
2024-04-12 08:11:08 -0400

    database file

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for univSite project.
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('students', views.students),
+    path('instructors', views.instructors, name='instructors'),
+    path('publication', views.publication, name='publication'),
+    path('admin', views.admin, name='admin'),
     path('admin/', admin.site.urls),
 ]
