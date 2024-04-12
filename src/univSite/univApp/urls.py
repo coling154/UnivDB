695c8d8d5d82b58c032ded4f4b174b780d06238a
Colin Gasiewicz <gasiewca@clarkson.edu>
2024-04-12 15:14:58 -0400

    added F2 view

--- src/univSite/univApp/urls.py
+++ src/univSite/univApp/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("index" , views.index, name="index"),
     path("admin/", views.admin, name="admin"),
+    path("admin/F1/", views.F1, name="F1"),
+    path("admin/F2/", views.F2, name="F2"),
+    path("admin/F3/", views.F3, name="F3"),
     path("professor/", views.professor, name="professor"),
     path("student/", views.student, name="student"),
 ]
--- src/univSite/univApp/views.py
+++ src/univSite/univApp/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import mysql.connector
+from django.db import connection
 
+from django.template import loader
 # Create your views here.
+
+def index(request):
+  return render(request, "index.html")
+
+
+def dictfetchall(cursor):
+    """
+    Return all rows from a cursor as a dict.
+    Assume the column names are unique.
+    """
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+
 def admin(request):
     print("ADMIN")
     return render(request, "admin.html")
@@ -11,3 +29,17 @@ def professor(request):
 
 def student(request):
     return render(request, "student.html")
+
+def F2(request):
+    dept = request.POST.get('dept', 0)
+    query = "SELECT dept_name,MIN(salary), MAX(salary), AVG(salary) FROM instructor GROUP BY dept_name HAVING dept_name = %s"
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query, (dept,))
+        data = dictfetchall(cursor)
+        print(data)
+    finally:
+        cursor.close()
+    return HttpResponse(data.__str__())
+
+
